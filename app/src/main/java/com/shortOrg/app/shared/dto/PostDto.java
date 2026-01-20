b4fa570ec9fdd5c87f6a6ee2b113d4d0f7bb9c54
남궁용진 <elelte991@tukorea.ac.kr>
2026-01-20 16:21:37 +0900

    post dto 추가

---
+++ app/src/main/java/com/shortOrg/app/shared/dto/PostDto.java
@@ -0,0 +1,27 @@
+package com.shortOrg.app.shared.dto;
+
+import com.shortOrg.app.domain.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class PostDto {
+    private Long id;
+    private String category;
+    private String title;
+    private String content;
+    private User writerId;
+    private LocalDateTime meetingTime;
+    private Double longitude;
+    private Double latitude;
+    private PostStatus state;
+    private JoinMode joinMode;
+    private LocalDateTime lastModified;
+}
