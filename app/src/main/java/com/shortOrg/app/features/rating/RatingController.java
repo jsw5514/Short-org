84c46349238dc642b7efed33bf219f737f3c6e31
남궁용진 <elelte991@tukorea.ac.kr>
2026-01-21 16:18:59 +0900

    오타 수정

--- app/src/main/java/com/shortOrg/app/features/rating/RatingController.java
+++ app/src/main/java/com/shortOrg/app/features/rating/RatingController.java
@@ -1,6 +1,5 @@
 package com.shortOrg.app.features.rating;
 
-import com.shortOrg.app.domain.Post;
 import com.shortOrg.app.shared.dto.RatingRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,8 +15,9 @@ public class RatingController {
     @PostMapping("/{postId}/ratings")
     public ResponseEntity<?> ratingEvaluation(@PathVariable Long postId, @RequestBody RatingRequest ratingRequest, Authentication auth) {
         String name = auth.getName();
-        ratingService.ratringEvaluation(postId, ratingRequest, name);
+        ratingService.ratingEvaluation(postId, ratingRequest, name);
 
         return ResponseEntity.ok("평가 성공");
     }
 }
+
--- app/src/main/java/com/shortOrg/app/features/rating/RatingService.java
+++ app/src/main/java/com/shortOrg/app/features/rating/RatingService.java
@@ -7,6 +7,7 @@ import com.shortOrg.app.repository.PostRepository;
 import com.shortOrg.app.repository.RatingRepository;
 import com.shortOrg.app.repository.UserRepository;
 import com.shortOrg.app.shared.dto.RatingRequest;
+import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,19 +19,20 @@ public class RatingService {
     private final RatingRepository ratingRepository;
     private final PostRepository postRepository;
     private final UserRepository userRepository;
+    private final EntityManager entityManager;
 
-    public void ratringEvaluation(Long postId, RatingRequest ratingRequest, String name) {
-        Post post = postRepository.findById(postId).orElseThrow(()->new RuntimeException("게시글 불러오기 실패"));
-        User raterId = userRepository.findById(name).orElseThrow(() -> new RuntimeException("평가하는 사람 불러오기 실패"));
-        User targetId = userRepository.findById(ratingRequest.getTargetUserId()).orElseThrow(()-> new RuntimeException("평가 받는 사람 불러오기 실패"));
-
-        boolean check = ratingRepository.existsByPostIdAndRaterIdAndTargetId(post, raterId, targetId);
+    public void ratingEvaluation(Long postId, RatingRequest ratingRequest, String name) {
+        boolean check = ratingRepository.existsByPostIdAndRaterIdAndTargetId(
+                entityManager.getReference(Post.class, postId),
+                (entityManager.getReference(User.class, name)),
+                (entityManager.getReference(User.class, ratingRequest.getTargetUserId()))
+        );
 
         if(!check){
             Rating rating = Rating.builder()
-                    .postId(post)
-                    .raterId(raterId)
-                    .targetId(targetId)
+                    .postId(entityManager.getReference(Post.class, postId))
+                    .raterId(entityManager.getReference(User.class, name))
+                    .targetId(entityManager.getReference(User.class, ratingRequest.getTargetUserId()))
                     .score(ratingRequest.getScore())
                     .comment(ratingRequest.getComment())
                     .build();
