f5e5c76fd42d03aeee4ac43a031d59d2826baa62
jsw5514 <77636044+jsw5514@users.noreply.github.com>
2026-01-22 18:18:17 +0900

    바뀐 db 데이터 타입에 맞게 거리 계산 쿼리 수정

--- app/src/main/java/com/shortOrg/app/repository/PostRepository.java
+++ app/src/main/java/com/shortOrg/app/repository/PostRepository.java
@@ -14,18 +14,32 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 
     List<Post> findByCategory(String category);
 
-    @Query(value = "SELECT * FROM post p " +
-            "WHERE (:radius IS NULL OR (6371 * acos(cos(radians(:lat)) * cos(radians(p.latitude)) * " +
-            "cos(radians(p.longitude) - radians(:lon)) + sin(radians(:lat)) * " +
-            "sin(radians(p.latitude)))) <= :radius / 1000.0) " +
-            "AND (:category IS NULL OR p.category = :category) " +
-            "ORDER BY (6371 * acos(cos(radians(:lat)) * cos(radians(p.latitude)) * " +
-            "cos(radians(p.longitude) - radians(:lon)) + sin(radians(:lat)) * " +
-            "sin(radians(p.latitude)))) ASC", nativeQuery = true)
+    /*
+    쿼리 설명
+    -사용한 함수
+    POINT(:lon, :lat) -> 입력받은 데이터를 나타내는 점
+    ST_SRID(..., 4326) -> 이 점은 SRID 4326(=WGS84, 구글맵에서 사용하는 좌표계)의 좌표계에 해당하는 점이라는 정보를 추가
+    ST_Distance_Sphere(...) -> 두 점 사이의 거리, 여기에선 DB에 저장된 좌표(p.location)과 입력받은 경위도 좌표(ST_SRID(POINT(:lon, :lat), 4326)) 간의 거리를 구했음
+    
+    -사용한 조건
+    ST_Distance_Sphere(...) <= radius --> 두 점 사이의 거리가 radius(지정한 반경 거리값, 미터)보다 작은 행만 검색(=반경 (radius)m 이내의 게시글만 검색)
+    
+    */
+    @Query(value = """
+        SELECT *
+        FROM (
+            SELECT p.*,
+                   ST_Distance_Sphere(p.location, ST_SRID(POINT(:lon, :lat), 4326)) AS distance
+            FROM post p
+            WHERE (:category IS NULL OR p.category = :category)
+        ) t
+        WHERE t.distance <= :radius
+        ORDER BY t.distance ASC
+    """, nativeQuery = true)
     List<Post> findNearByPosts(
-            @Param("lon") Double lon,
-            @Param("lat") Double lat,
-            @Param("radius") Integer radius,
+            @Param("lon") double lon,
+            @Param("lat") double lat,
+            @Param("radius") int radius,
             @Param("category") String category
     );
 }
