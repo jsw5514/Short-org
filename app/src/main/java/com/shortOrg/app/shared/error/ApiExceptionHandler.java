f8f6d2e5033cdc3b402b1ed03e7eecd3ae57f5ad
jsw5514 <77636044+jsw5514@users.noreply.github.com>
2026-01-22 18:22:32 +0900

    명시적 처리대상 예외 추가

--- app/src/main/java/com/shortOrg/app/shared/error/ApiExceptionHandler.java
+++ app/src/main/java/com/shortOrg/app/shared/error/ApiExceptionHandler.java
@@ -3,8 +3,11 @@ package com.shortOrg.app.shared.error;
 import com.shortOrg.app.shared.dto.ErrorResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @RestControllerAdvice
 public class ApiExceptionHandler {
@@ -13,10 +16,30 @@ public class ApiExceptionHandler {
         return ResponseEntity.status(415).body(err("Not Supported Media Type", e.getMessage()));
     }
     
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<?> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException e) {
+        return ResponseEntity.badRequest().body(err("Not Supported Argument Type", e.getMessage()));
+    }
+    
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<?> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException e) {
+        return ResponseEntity.status(405).body(err("Not Supported Method Type", e.getMessage()));
+    }
+    
+    @ExceptionHandler({ServletRequestBindingException.class, MethodArgumentTypeMismatchException.class})
+    public ResponseEntity<?> handleServletRequestBindingException(Throwable e) {
+        return ResponseEntity.badRequest().body(err("Invalid Request Content", e.getMessage()));
+    }
+    
+    @ExceptionHandler(TokenReusedException.class)
+    public ResponseEntity<?> handleTokenReused(TokenReusedException e) {
+        return ResponseEntity.status(401).body(err("Unauthorized", "Invalid Refresh Token. Please login again."));
+    }
+    
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleAny(Exception e) {
         e.printStackTrace();
-        return ResponseEntity.status(500).body(err("Internal Server Error", "서버 로그 확인할것"));
+        return ResponseEntity.status(500).body(err("Internal Server Error", "서버 로그를 확인해주세요."));
     }
     
     private ErrorResponse err(String code, String message) {
