bba0fac1399f57cd0fb0f9fb9b149c5883255f10
남궁용진 <>
2026-01-23 17:20:54 +0900

    PostMapper 구조 변경

--- app/src/main/java/com/shortOrg/app/features/post/mapper/PostMapper.java
+++ app/src/main/java/com/shortOrg/app/features/post/mapper/PostMapper.java
@@ -4,23 +4,30 @@ import com.shortOrg.app.domain.Post;
 import com.shortOrg.app.domain.User;
 import com.shortOrg.app.features.post.dto.PostCreateRequest;
 import com.shortOrg.app.features.post.dto.PostResponse;
+import com.shortOrg.app.features.user.dto.UserSummary;
+import com.shortOrg.app.features.user.mapper.UserMapper;
 import com.shortOrg.app.shared.enumerate.PostStatus;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class PostMapper {
     private final EntityManager entityManager;
+    private final UserMapper userMapper;
 
     public PostResponse fromEntity(Post post) {
+        UserSummary userSummary = userMapper.fromEntity(post.getWriter());
+
         return PostResponse.builder()
                 .id(post.getId())
                 .category(post.getCategory())
                 .title(post.getTitle())
                 .content(post.getContent())
-                .writer(post.getWriter())
+                .writer(userSummary)
                 .meetingTime(post.getMeetingTime())
                 .locationName(post.getLocationName())
                 .longitude(post.getLongitude())
