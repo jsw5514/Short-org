89a488b7de93262442d477697f44e219110884e1
jsw5514 <77636044+jsw5514@users.noreply.github.com>
2026-01-26 14:20:37 +0900

    오류가 발생했을때 스택 트레이스를 서버 로그로 출력하도록 수정

--- app/src/main/java/com/shortOrg/app/shared/error/ApiExceptionHandler.java
+++ app/src/main/java/com/shortOrg/app/shared/error/ApiExceptionHandler.java
@@ -13,33 +13,38 @@ import org.springframework.web.method.annotation.MethodArgumentTypeMismatchExcep
 public class ApiExceptionHandler {
     @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
     public ResponseEntity<?> handleMediaTypeError(HttpMediaTypeNotSupportedException e) {
+        e.printStackTrace();
         return ResponseEntity.status(415).body(err("Not Supported Media Type", e.getMessage()));
     }
     
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity<?> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException e) {
+        e.printStackTrace();
         return ResponseEntity.badRequest().body(err("Not Supported Argument Type", e.getMessage()));
     }
     
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     public ResponseEntity<?> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException e) {
+        e.printStackTrace();
         return ResponseEntity.status(405).body(err("Not Supported Method Type", e.getMessage()));
     }
 
     @ExceptionHandler(ServletRequestBindingException.class)
     public ResponseEntity<?> handleServletRequestBindingException(ServletRequestBindingException e) {
+        e.printStackTrace();
         return ResponseEntity.badRequest().body(err("Invalid Request Content", e.getMessage()));
     }
     
     @ExceptionHandler(TokenReusedException.class)
     public ResponseEntity<?> handleTokenReused(TokenReusedException e) {
+        e.printStackTrace();
         return ResponseEntity.status(401).body(err("Unauthorized", "Invalid Refresh Token. Please login again."));
     }
     
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleAny(Exception e) {
         e.printStackTrace();
-        return ResponseEntity.status(500).body(err("Internal Server Error", "서버 로그를 확인해주세요."));
+        return ResponseEntity.status(500).body(err("Internal Server Error", "알 수 없는 오류입니다. 서버 로그를 확인해주세요."));
     }
     
     private ErrorResponse err(String code, String message) {
